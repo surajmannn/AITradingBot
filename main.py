69bd5236e6443a28958437625bb4cad0a955304f
Suraj Mann <surajmann@MacBook-S.local>
2024-08-15 00:46:41 +0100

    Final commit for today, more updates.

--- main.py
+++ main.py
@@ -25,10 +25,7 @@ def testing():
             interval='1m',                # The trading interval (1minute required for now)
             confidence_level=0.5,         # Confidence level for the machine learning probability (trade execution)
             desired_model=1,              # The specific model to use for simulation (1=MLP, 2=SVM, 3=RF, 4=LSTM)
-            start_date='2024-07-01',      # The start date for the simulation
-            end_date='2024-08-12',        # The end date for the simulation
-            training_range=1,             # The range of training data in days
-            simulation_range=5            # The range of the simulation in days (maximum 17)
+            simulation_range=6            # The range of the simulation in days (maximum 17)
     )
     balance = GBPUSD.run_trading_simulation()
     print("\nFinal Balance: ", balance)
