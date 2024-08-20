4f46879b709ec615ad16694d6756b0c0144c9f09
Suraj Mann <surajmann@MacBook-S.local>
2024-08-20 23:41:22 +0100

    Slight tweaks for extra columns in database.

--- trading_system/ml_performance.py
+++ trading_system/ml_performance.py
@@ -3,8 +3,8 @@
 from database_models.ml_metrics import *
 
 # This function sends a buy execution into the database
-def add_metrics(ticker, ml_model, training_start_date, training_end_date, accuracy, roc_auc, sig_accuracy, roc_auc_accuracy):
+def add_metrics(ticker, ml_model, training_start_date, training_end_date, accuracy, roc_auc, sig_accuracy, sig_roc_auc):
     userID = 1          # irrelevant for test scenario
-    values = (userID, ticker, ml_model, training_start_date, training_end_date, accuracy, roc_auc, sig_accuracy, roc_auc_accuracy)
+    values = (userID, ticker, ml_model, training_start_date, training_end_date, accuracy, roc_auc, sig_accuracy, sig_roc_auc)
     data = add_performance_metrics(values)     # database handler call
     return data
